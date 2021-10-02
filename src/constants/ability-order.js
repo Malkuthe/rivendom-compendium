b9335ca23e87099fdc2e139590610a442ce33420
John Baltao <baltaojp@myumanitoba.ca>
2021-10-02 09:02:07 -0500

    Directory restructuring. Making sure to ignore node_modules in both client and server directories

--- src/constants/ability-order.js
+++
@@ -1,8 +0,0 @@
-export default [
-  'str',
-  'dex',
-  'con',
-  'int',
-  'wis',
-  'cha',
-];
--- src/plugins/constants.js
+++
@@ -1,9 +0,0 @@
-import crxptable from '../constants/cr-xp-table';
-import abilityOrder from '../constants/ability-order';
-
-export default {
-  install: (app) => {
-    app.provide('crxptable', crxptable);
-    app.provide('abilityOrder', abilityOrder);
-  },
-};
