c65adbf781e6782c2ecf00754be094aec13ab97d
John Baltao <jp.baltao@gmail.com>
2021-11-01 21:50:35 -0500

    Added handling for 5etools requests

--- server/index.js
+++ server/index.js
@@ -18,6 +18,21 @@ app.get('/api/creature/:creature_id', (req, res) => {
     })
 });
 
+app.get('/api/5etools/creature/:creature_id(\\.json)?', (req, res) => {
+  const id = req.params.creature_id;
+  axios.get(`${process.env.API_ENDPOINT}/creature/${id}`, {
+    headers: {
+      'X-Api-Key': process.env.API_KEY,
+    }
+  })
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`App now listening at port ${process.env.PORT}`);
 });
